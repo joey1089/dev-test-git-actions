744e0445bf2c1f5b5fdacff4897d0a0c2e716b43
Joseph Peter <90427049+joey1089@users.noreply.github.com>
2023-03-02 21:34:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 def done():
-  action()
-  print("All done")
+    action()
+    print("All done")
 
 
 def action():
-  print("action is happening here")
+    print("action is happening here")
 
 
 print(done())
